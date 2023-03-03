b1ebd55a01d99e06dc53e9b8677261dc3794c090
Drimodaren <men.a.a@yandex.ru>
2023-03-03 18:42:10 +0400

    #4 filling adapter

---
+++ src/services/todoAdapter.js
@@ -0,0 +1,17 @@
+export const getAllTodos = async () => {
+  return await [
+    { id: 1, title: 'first', completed: false },
+    { id: 2, title: 'second', completed: false },
+    { id: 3, title: 'third', completed: true },
+  ];
+};
+export const addNewTodo = async (newPartialTodo) => {
+  const newTodo = { ...newPartialTodo, id: new Date() };
+  return newTodo;
+};
+export const updateTodoById = async (id, newTodo) => {
+  return newTodo;
+};
+export const deleteTodo = async (id) => {
+  return id;
+};
