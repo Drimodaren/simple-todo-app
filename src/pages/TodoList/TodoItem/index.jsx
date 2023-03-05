8603c154fe314f391a3fa52c39bfebf7f4819502
Drimodaren <men.a.a@yandex.ru>
2023-03-05 15:31:25 +0400

    #10 Add data and function in TodoItem

--- src/pages/TodoList/TodoItem/index.jsx
+++ src/pages/TodoList/TodoItem/index.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { TodoContext } from '../../../context';
 
-export default function TodoItem({title,completed}) {
+export default function TodoItem({ id, title, completed }) {
+  const { toggleCompleted, deleteTodo } = useContext(TodoContext);
+
+  const handleToggle = (id) => {
+    toggleCompleted(id);
+  };
+  const handleDelete = (id) => {
+    deleteTodo(id);
+  };
   return (
-    <div>{title}</div>
-  )
+    <div>
+      {title}
+
+      <input type="checkbox" completed={completed} onChange={handleToggle} />
+      <button onClick={handleDelete}>delete</button>
+    </div>
+  );
 }
